2a92ec59e6d8c70a41526c4d6b4a5e52d7f6c1e3
cl.zhan <cl.zhan@c70d9b38-4d0c-4fe8-a536-286820d0cac4>
2016-01-08 04:36:51 +0000

    add telepresence source code
    Signed-off-by: cl.zhan <cl.zhan@suirui.com>
    
    git-svn-id: http://192.168.90.100/svn/1-CodeLib/01.SDK/mcuforRecorder/trunk@1315 c70d9b38-4d0c-4fe8-a536-286820d0cac4

---
+++ telepresence/config.h
@@ -0,0 +1,175 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <AL/alc.h> header file. */
+/* #undef HAVE_AL_ALC_H */
+
+/* Define to 1 if you have the <AL/alext.h> header file. */
+/* #undef HAVE_AL_ALEXT_H */
+
+/* Define to 1 if you have the <AL/al.h> header file. */
+/* #undef HAVE_AL_AL_H */
+
+/* Define to 1 if you have the <com/sun/star/bridge/XUnoUrlResolver.hpp>
+   header file. */
+/* #undef HAVE_COM_SUN_STAR_BRIDGE_XUNOURLRESOLVER_HPP */
+
+/* Define to 1 if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1
+
+/* Define to 1 if you have the `execl' function. */
+#define HAVE_EXECL 1
+
+/* HAVE_FFMPEG */
+#define HAVE_FFMPEG 1
+
+/* Define to 1 if you have the `fork' function. */
+#define HAVE_FORK 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <libavcodec/avcodec.h> header file. */
+/* #undef HAVE_LIBAVCODEC_AVCODEC_H */
+
+/* Define to 1 if you have the <libavfilter/avfilter.h> header file. */
+/* #undef HAVE_LIBAVFILTER_AVFILTER_H */
+
+/* Define to 1 if you have the <libavformat/avformat.h> header file. */
+/* #undef HAVE_LIBAVFORMAT_AVFORMAT_H */
+
+/* Define to 1 if you have the <libavutil/avutil.h> header file. */
+/* #undef HAVE_LIBAVUTIL_AVUTIL_H */
+
+/* Define to 1 if you have the <libswresample/swresample.h> header file. */
+/* #undef HAVE_LIBSWRESAMPLE_SWRESAMPLE_H */
+
+/* Define to 1 if you have the <libswscale/swscale.h> header file. */
+/* #undef HAVE_LIBSWSCALE_SWSCALE_H */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* HAVE_OPENAL */
+#define HAVE_OPENAL 1
+
+/* HAVE_OPENOFFICE */
+/* #undef HAVE_OPENOFFICE */
+
+/* Define to 1 if you have the `pclose' function. */
+#define HAVE_PCLOSE 1
+
+/* Define to 1 if you have the `popen' function. */
+#define HAVE_POPEN 1
+
+/* Define to 1 if you have the `posix_spawn' function. */
+#define HAVE_POSIX_SPAWN 1
+
+/* Define to 1 if you have the `setpgid' function. */
+#define HAVE_SETPGID 1
+
+/* Define to 1 if you have the <spawn.h> header file. */
+#define HAVE_SPAWN_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <sys/wait.h> header file. */
+#define HAVE_SYS_WAIT_H 1
+
+/* Define to 1 if you have the <tinybfcp/tinybfcp_config.h> header file. */
+/* #undef HAVE_TINYBFCP_TINYBFCP_CONFIG_H */
+
+/* Define to 1 if you have the <tinydav/tinydav_config.h> header file. */
+/* #undef HAVE_TINYDAV_TINYDAV_CONFIG_H */
+
+/* Define to 1 if you have the <tinyhttp/tinyhttp_config.h> header file. */
+/* #undef HAVE_TINYHTTP_TINYHTTP_CONFIG_H */
+
+/* Define to 1 if you have the <tinyipsec/tinyipsec_config.h> header file. */
+/* #undef HAVE_TINYIPSEC_TINYIPSEC_CONFIG_H */
+
+/* Define to 1 if you have the <tinymedia/tinymedia_config.h> header file. */
+/* #undef HAVE_TINYMEDIA_TINYMEDIA_CONFIG_H */
+
+/* Define to 1 if you have the <tinymsrp/tinymsrp_config.h> header file. */
+/* #undef HAVE_TINYMSRP_TINYMSRP_CONFIG_H */
+
+/* Define to 1 if you have the <tinynet/tinynet_config.h> header file. */
+/* #undef HAVE_TINYNET_TINYNET_CONFIG_H */
+
+/* Define to 1 if you have the <tinyrtp/tinyrtp_config.h> header file. */
+/* #undef HAVE_TINYRTP_TINYRTP_CONFIG_H */
+
+/* Define to 1 if you have the <tinysak/tinysak_config.h> header file. */
+/* #undef HAVE_TINYSAK_TINYSAK_CONFIG_H */
+
+/* Define to 1 if you have the <tinysdp/tinysdp_config.h> header file. */
+/* #undef HAVE_TINYSDP_TINYSDP_CONFIG_H */
+
+/* Define to 1 if you have the <tinysigcomp/tinysigcomp_config.h> header file.
+   */
+/* #undef HAVE_TINYSIGCOMP_TINYSIGCOMP_CONFIG_H */
+
+/* Define to 1 if you have the <tinysip/tinysip_config.h> header file. */
+/* #undef HAVE_TINYSIP_TINYSIP_CONFIG_H */
+
+/* Define to 1 if you have the <tinysms/tinysms_config.h> header file. */
+/* #undef HAVE_TINYSMS_TINYSMS_CONFIG_H */
+
+/* Define to 1 if you have the <tinyxcap/tinyxcap_config.h> header file. */
+/* #undef HAVE_TINYXCAP_TINYXCAP_CONFIG_H */
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `vfork' function. */
+#define HAVE_VFORK 1
+
+/* Define to 1 if you have the `waitpid' function. */
+#define HAVE_WAITPID 1
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "telepresence"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "opentelepresence(at)googlegroups(dot)com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "telepresence"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "telepresence 2.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "telepresence"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "2.1"
